45cd57b47b9542001d730eaabadd88234e47542d
Pacihstan <pax@isphere.net>
2024-02-09 09:27:46 -0600

    Added secondary SMTP program

--- SMTP/SMTP.py
+++ SMTP/SMTP.py
@@ -3,7 +3,6 @@ msg = "\r\n I love computer networks!"
 endmsg = "\r\n.\r\n"
 # Choose a mail server (e.g. Google mail server) and call it mailserver
 mailserver = "smtp.gmail.com"
-s = smtplib.SMTP('smtp.gmail.com', 587)
 
 # Create socket called clientSocket and establish a TCP connection with mailserver
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -30,22 +29,26 @@ print(recv)
 # Send RCPT TO command and print server response.
 rcptCommand = 'RCPT TO pax@isphere.net\r\n'
 clientSocket.send(rcptCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
 
 # Send DATA command and print server response.
-dataCommand = """DATA\r\n',
-            Date: Sun, 30 Nov 98 nn:nn:nn EST\r\n,
-            From: Paxton <pax@isphere.net>\r\n,
-            To: <pax@isphere.net>\r\n,
-            Subject: Test\r\n\r\n,
-            This is a test message.\r\n"""
+dataCommand = 'DATA\r\n'
+clientSocket.send(dataCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
 # Send message data.
-clientSocket.send(dataCommand.encode())
+clientSocket.send(msg.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
 # Message ends with a single period.
-clientSocket.send('.\r\n'.encode())
+clientSocket.send(endmsg.encode())
 
 # Send QUIT command and get server response.
 quitCommand = 'QUIT\r\n'
 clientSocket.send(quitCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
