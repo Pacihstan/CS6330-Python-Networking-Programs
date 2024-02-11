b6ebc808d173af32191c1eccee12e91f548d036a
Pacihstan <pax@isphere.net>
2024-02-11 11:37:35 -0600

    Updated testing files

--- ProxyServer/ProxyOnce.py
+++ ProxyServer/ProxyOnce.py
@@ -1,10 +1,10 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
-tcpSerSock.bind(('', 8888))
+tcpSerSock.bind(('', 8887))
 tcpSerSock.listen(5)
 
-for i in range(5):
+while True:
     print('Ready to serve...')
     tcpCliSock, addr = tcpSerSock.accept()
     print('Received connection from:', addr)
@@ -14,33 +14,64 @@ for i in range(5):
     except:
         print('#failed to decode message')
         continue
+    
+    
+
 
     # Extract filename
+    filename = ''
+    port = 80
     try:
-        print(message.split()[1])
-        filename = message.split()[1].split("/")[1]
+        if ("http://" in message):
+            filename = message.split()[1].split("/")[3]
+        elif ("https://" in message):
+            filename = message.split()[1].split("/")[3]
+        elif "/" in message:
+            filename = message.split()[1].split("/")[0]
+        else:
+            filename = message.split()[1]
+        #this is a test, remove later
+        port = filename.split(":")[1]
+        print("Port:"+port)
+        filename = filename.split(":")[0]
         print('successfully split message')
     except:
         print('#Failed to split message')
         filename = ''
+        continue
     print('Filename:' + filename)
+    port = int(port)
+
 
     c = socket(AF_INET, SOCK_STREAM)
     hostn = filename.replace("www.", "", 1)
-    print('Hostname: ' + hostn)
-    c.connect((hostn, 80))
+    print('Hostname:' + hostn)
+    #c.connect((hostn.encode(), port))
+    #c.send(message.encode())
+
+    #test a perfectly reasonable exchange
+    c.connect((hostn,port))
+    c.send(b"GET / HTTP/1.1\r\nHost:https://"+hostn.encode()+b"\r\n\r\n")
 
     # Formulate and send HTTP request
-    request = "GET /" + filename + " HTTP/1.1\r\nHost: " + hostn + "\r\n\r\n"
-    c.sendall(request.encode())
+    #request = message
+    #c.sendall(request.encode())
 
     # Receive and process HTTP response
+    c.settimeout(3)
     response = b""
     while True:
-        data = c.recv(1024)
-        if not data:
+        try:
+            data = c.recv(1024)
+            if not data:
+                break
+            response += data
+        except:
+            print('Timeout occurred...')
             break
-        response += data
+
+    #print response
+    print('Response message:' + response.decode())
 
     # Send response to client
     tcpCliSock.sendall(response)
